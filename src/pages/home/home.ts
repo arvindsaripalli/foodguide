bc2a0b9485c961c768a8d268f5c49b06c0abf3af
Arvind Saripalli <mail2asrs@gmail.com>
2017-09-12 16:14:58 -0700

    Added Barcode Scanning functionality using the BarcodeScanner Ionic Native Plugin.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public barcodeScanner: BarcodeScanner,
+    public toastCtrl: ToastController) {
 
   }
 
+  scanBarcode() {
+    this.barcodeScanner.scan().then((barcodeData) => {
+      //Found UPC
+      console.log("UPC found");
+      console.log(JSON.stringify(barcodeData));
+
+      //Move on to DB checking stage before displaying product modal
+    }, (err) => {
+      console.log("Scan failed");
+      console.log(JSON.stringify(err));
+      this.showToast("Scan failed. Please try again.");
+    });
+  }
+
+  searchDB() {
+
+  }
+
+  showToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000
+    });
+    toast.present();
+  }
 }
