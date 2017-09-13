6cd7e73f0f6df07a9a3454e62e3ab31008027b20
Arvind Saripalli <mail2asrs@gmail.com>
2017-09-12 17:31:43 -0700

    Scanned UPC now calls a GET request to the USDA NutritionDB. Properly error handles in the case that a product UPC is valid and scanned but is not a USDA foodstuff. Opens a new modal and has all nutrition data from valid UPCs.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,31 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { HttpModule } from '@angular/http';
+
 
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ProductPage } from '../pages/product/product';
 
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ProductPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ProductPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
-import { NavController, ToastController } from 'ionic-angular';
+import { NavController, ToastController, ModalController } from 'ionic-angular';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Http, Headers } from '@angular/http';
+
+import { ProductPage } from '../product/product';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  private usda_api = "BnFxHw2boWUzPCneV3EyXOhPUKRPY31hDwzCSbSW";
   constructor(
     public navCtrl: NavController,
     public barcodeScanner: BarcodeScanner,
-    public toastCtrl: ToastController) {
+    public toastCtrl: ToastController,
+    public http: Http,
+    public modalCtrl: ModalController) {
 
   }
 
@@ -22,6 +27,7 @@ export class HomePage {
       console.log(JSON.stringify(barcodeData));
 
       //Move on to DB checking stage before displaying product modal
+      this.searchFromUPC(barcodeData.text);
     }, (err) => {
       console.log("Scan failed");
       console.log(JSON.stringify(err));
@@ -29,8 +35,51 @@ export class HomePage {
     });
   }
 
-  searchDB() {
+  searchFromUPC(upc) {
+    let headers = new Headers();
+    headers.append('Accept', 'application/json');
 
+    this.http.get("https://api.nal.usda.gov/ndb/search/?format=json&q=" + upc + "&max=25&offset=0&api_key=" + this.usda_api, {headers: headers}).subscribe(
+      (res) => {
+        var response = JSON.parse(res["_body"]);
+
+        if(response["errors"] != undefined) {
+          console.log("Search from UPC API empty");
+          this.showToast("We could not find your product in the USDA Nutrition Database.");
+        } else {
+          console.log("Search from UPC API successful");
+          console.log(response["list"]["item"]);
+          this.getFromID(response["list"]["item"][0]["ndbno"]);
+        }
+      }, (err) => {
+        console.log("Search from UPC API unsuccessful");
+        console.log(JSON.stringify(err));
+      }
+    );
+  }
+
+  getFromID(ndbno) {
+    let headers = new Headers();
+    headers.append('Accept', 'application/json');
+
+    this.http.get("https://api.nal.usda.gov/ndb/V2/reports?ndbno=" + ndbno + "&api_key=" + this.usda_api, {headers: headers}).subscribe(
+      (res) => {
+        var response = JSON.parse(res["_body"]);
+
+        console.log("Search from UPC API successful");
+        console.log(JSON.stringify(response));
+
+        this.showProduct(response);
+
+      }, (err) => {
+        console.log(JSON.stringify(err));
+      }
+    );
+  }
+
+  showProduct(response) {
+    let modal = this.modalCtrl.create(ProductPage, {product: response});
+    modal.present();
   }
 
   showToast(msg) {
